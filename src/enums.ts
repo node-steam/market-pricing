c1f95870e12908f6792fe16f81f9608c384c207d
Silas Rech <silas@panascais.io>
2017-05-06 02:33:05 +0200

    Add all games who have items on the market

--- src/enums.ts
+++ src/enums.ts
@@ -411,13 +411,97 @@ export const CurrencySign: { [key: string]: string } = {
 
 export enum Application {
     /**
+     * [Alien Hostage](https://steamdb.info/app/562430/)
+     */
+    ALIEN_HOSTAGE = 562430,
+    /**
+     * [Altitude0: Lower & Faster](https://steamdb.info/app/308080/)
+     */
+    ALTITUDE0 = 308080,
+    /**
+     * [- Arcane Raise -](https://steamdb.info/app/603750/)
+     */
+    ARCANE_RAISE = 603750,
+    /**
+     * [Armello](https://steamdb.info/app/290340/)
+     */
+    ARMELLO = 290340,
+    /**
+     * [.atorb.](https://steamdb.info/app/467530/)
+     */
+    ATORB = 467530,
+    /**
+     * [Ballistic Overkill](https://steamdb.info/app/296300/)
+     */
+    BALLISTIC_OVERKILL = 296300,
+    /**
+     * [BattleBlock Theater](https://steamdb.info/app/238460/)
+     */
+    BATTLEBLOCK_THEATER = 238460,
+    /**
+     * [BATTLECREW Space Pirates](https://steamdb.info/app/411480/)
+     */
+    BATTLECREW_SPACE_PIRATES = 411480,
+    /**
+     * [Bloody Walls](https://steamdb.info/app/531960/)
+     */
+    BLOODY_WALLS = 531960,
+    /**
+     * [Business Tour](https://steamdb.info/app/397900/)
+     */
+    BUSINESS_TOUR = 397900,
+    /**
+     * [Call to Arms](https://steamdb.info/app/302670/)
+     */
+    CALL_TO_ARMS = 302670,
+    /**
+     * [Children of Orc](https://steamdb.info/app/544840/)
+     */
+    CHILDREN_OF_ORC = 544840,
+    /**
      * [Counter-Strike: Global Offensive](https://steamdb.info/app/730/)
      */
     CSGO = 730,
     /**
+     * [Depth](https://steamdb.info/app/274940/)
+     */
+    DEPTH = 274940,
+    /**
+     * [Destinations](https://steamdb.info/app/453170/)
+     */
+    DESTINATIONS = 453170,
+    /**
+     * [Don't Starve Together](https://steamdb.info/app/322330/)
+     */
+    DONT_STARVE_TOGETHER = 322330,
+    /**
      * [Dota 2](https://steamdb.info/app/570/)
      */
     DOTA2 = 570,
+    /**
+     * [El Ninja (Beta)](https://steamdb.info/app/509840/)
+     */
+    EL_NINJA_BETA = 509840,
+    /**
+     * [Ember Strike](https://steamdb.info/app/374670/)
+     */
+    EMBER_STRIKE = 374670,
+    /**
+     * [Forgotten Lore](https://steamdb.info/app/391240/)
+     */
+    FORGOTTEN_LORE = 391240,
+    /**
+     * [FreeCell Quest](https://steamdb.info/app/364640/)
+     */
+    FREECELL_QUEST = 364640,
+    /**
+     * [Fruit Ninja VR](https://steamdb.info/app/486780/)
+     */
+    FRUIT_NINJA_VR = 486780,
+    /**
+     * [Gremlins, Inc.](https://steamdb.info/app/369990/)
+     */
+    GREMLINS_INC = 369990,
     /**
      * [H1Z1: Just Survive](https://steamdb.info/app/295110/)
      */
@@ -427,9 +511,45 @@ export enum Application {
      */
     H1Z1_KINGOFTHEKILL = 433850,
     /**
+     * [Heliborne](https://steamdb.info/app/433530/)
+     */
+    HELIBORNE = 433530,
+    /**
+     * [Immune](https://steamdb.info/app/348670/)
+     */
+    IMMUNE = 348670,
+    /**
+     * [INTERSHELTER](https://steamdb.info/app/520530/)
+     */
+    INTERSHELTER = 520530,
+    /**
+     * [Intralism](https://steamdb.info/app/513510/)
+     */
+    INTRALISM = 513510,
+    /**
      * [Killing Floor 2](https://steamdb.info/app/232090/)
      */
     KILLING_FLOOR2 = 232090,
+    /**
+     * [MegaRats](https://steamdb.info/app/534210/)
+     */
+    MEGARATS = 534210,
+    /**
+     * [MineSweeper VR](https://steamdb.info/app/516940/)
+     */
+    MINESWEEPER_VR = 516940,
+    /**
+     * [Move or Die](https://steamdb.info/app/323850/)
+     */
+    MOVE_OR_DIE = 323850,
+    /**
+     * [Natural Selection 2](https://steamdb.info/app/4920/)
+     */
+    NATURAL_SELECTION2 = 4920,
+    /**
+     * [Path of Exile](https://steamdb.info/app/238960/)
+     */
+    PATH_OF_EXILE = 238960,
     /**
      * [PAYDAY 2](https://steamdb.info/app/218620/)
      */
@@ -439,15 +559,91 @@ export enum Application {
      */
     PLAYERUNKNOWNS_BATTLEGROUNDS = 578080,
     /**
+     * [Primal Carnage: Extinction](https://steamdb.info/app/321360/)
+     */
+    PRIMAL_CARNAGE_EXTINCTION = 321360,
+    /**
+     * [Project Lounge](https://steamdb.info/app/508710/)
+     */
+    PROJECT_LOUNGE = 508710,
+    /**
+     * [Ratz Instagib 2.0](https://steamdb.info/app/338170/)
+     */
+    RATZ_INSTAGIB2 = 338170,
+    /**
+     * [Redout: Enhanced Edition](https://steamdb.info/app/517710/)
+     */
+    REDOUT_ENHANCED_EDITION = 517710,
+    /**
+     * [Reflex Arena](https://steamdb.info/app/328070/)
+     */
+    REFLEX_ARENA = 328070,
+    /**
+     * [Robot Roller-Derby Disco Dodgeball](https://steamdb.info/app/270450/)
+     */
+    ROBOT_ROLLER_DERBY_DISCO_DODGEBALL = 270450,
+    /**
      * [Rust](https://steamdb.info/app/252490/)
      */
     RUST = 252490,
+    /**
+     * [Savage Resurrection](https://steamdb.info/app/366440/)
+     */
+    SAVAGE_RESURRECTION = 366440,
+    /**
+     * [Screeps](https://steamdb.info/app/464350/)
+     */
+    SCREEPS = 464350,
+    /**
+     * [Slymes](https://steamdb.info/app/530300/)
+     */
+    SLYMES = 530300,
+    /**
+     * [SNOW](https://steamdb.info/app/244930/)
+     */
+    SNOW = 244930,
+    /**
+     * [SpeedRunners](https://steamdb.info/app/207140/)
+     */
+    SPEEDRUNNERS = 207140,
+    /**
+     * [Spellsworn](https://steamdb.info/app/360620/)
+     */
+    SPELLSWORN = 360620,
+    /**
+     * [Steam](https://steamdb.info/app/753/)
+     */
+    STEAM = 753,
+    /**
+     * [Subnautica](https://steamdb.info/app/264710/)
+     */
+    SUBNAUTICA = 264710,
+    /**
+     * [Supraball](https://steamdb.info/app/321400/)
+     */
+    SUPRABALL = 321400,
     /**
      * [Team Fortress 2](https://steamdb.info/app/440/)
      */
     TF2 = 440,
     /**
+     * [The Culling](https://steamdb.info/app/437220/)
+     */
+    THE_CULLING = 437220,
+    /**
      * [Unturned](https://steamdb.info/app/304930/)
      */
     UNTURNED = 304930,
+    /**
+     * [Wallpaper Engine](https://steamdb.info/app/431960/)
+     */
+    WALLPAPER_ENGINE = 431960,
+    /**
+     * [Warframe](https://steamdb.info/app/230410/)
+     */
+    WARFRAME = 230410,
+    /**
+     * [Zombie Grinder](https://steamdb.info/app/263920/)
+     */
+    ZOMBIE_GRINDER = 263920,
 }
